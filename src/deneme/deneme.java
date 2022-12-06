40985f18afd00620fb94f440ec15dbe5aa8a9e57
adnanortakoy <113705630+adnanortakoy@users.noreply.github.com>
2022-12-06 22:36:26 +0300

    1. versiyon

--- src/deneme/deneme.java
+++ src/deneme/deneme.java
@@ -1,7 +1,8 @@
 package deneme;
 
 public class deneme {
-    public static void main(String[] args) {
+    public static <string> void main(String[] args) {
         System.out.println("\"hello world\" = " + "hello world");
-    }
-}
+
+
+    }}
